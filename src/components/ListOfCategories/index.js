d9417028a993382763b858e86458d0375b0addd8
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-09 18:34:00 +0200

    chore: fetch list of category from api and remove mock

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Category} from "../Category";
 import {List, Item} from "./styles";
-import {categories} from "../../../api/db.json";
+import {API_BASE_URL} from "../../constants";
 
 
 export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(function(){
+    fetch(`${API_BASE_URL}/categories`)
+      .then(res => res.json())
+      .then(response => setCategories(response));
+  }, []);
   return (
     <List>
       {
