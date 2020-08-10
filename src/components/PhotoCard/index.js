219d0837483fdff0cd64065ecf4f673d430806db
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-10 17:42:08 +0200

    chore: add lazy loading with IntersectionObserver and react hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,40 @@
-import React from "react";
-import {Img, ImgWrapper, Button} from "./styles";
+import React, {Fragment, useEffect, useRef, useState} from "react";
+import {Img, ImgWrapper, Button, Article} from "./styles";
 import {MdFavoriteBorder} from "react-icons/md";
 
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(function(){
+    const observer = new window.IntersectionObserver(function(entries){
+      const { isIntersecting } = entries[0];
+      if(isIntersecting){
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(ref.current);
+  }, [ref]);
+
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt="image"/>
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt="image"/>
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </Fragment>
+      }
+    </Article>
   )
 }
