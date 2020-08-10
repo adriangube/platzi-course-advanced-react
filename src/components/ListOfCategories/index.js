dc742e4eacf51467f9ca809e029acf82da562181
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-10 17:21:11 +0200

    chore: add useCategoriesData custom hook

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,24 +1,59 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import {Category} from "../Category";
 import {List, Item} from "./styles";
 import {API_BASE_URL} from "../../constants";
 
 
-export const ListOfCategories = () => {
+export function useCategoriesData (){
   const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(function(){
+    setLoading(true);
     fetch(`${API_BASE_URL}/categories`)
       .then(res => res.json())
-      .then(response => setCategories(response));
+      .then(response => {
+        setCategories(response);
+        setLoading(false);
+      });
   }, []);
-  return (
-    <List>
+
+  return { categories, loading };
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData();
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(function(){
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    }
+    document.addEventListener('scroll', onScroll);
+
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
+        loading  ?
+          <Item key="loading">
+            <Category/>
+          </Item>  :
         categories.map(category =>
           <Item key={category.id}>
-           <Category {...category} />
+            <Category {...category} />
           </Item>)
       }
     </List>
+  )
+
+  return (
+    <Fragment>
+      {renderList()}
+      { showFixed && renderList(true)}
+    </Fragment>
   )
 }
