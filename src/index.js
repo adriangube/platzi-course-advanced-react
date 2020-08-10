99bd1a55b642d7d0723f8efc110186ad7ef32a54
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-10 20:03:29 +0200

    chore: add graphql and react-apollo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import { App } from "./App";
+import {ApolloClient, HttpLink} from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+import {API_BASE_URL} from "./constants";
+import {InMemoryCache} from "apollo-cache-inmemory";
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+const cache = new InMemoryCache()
+const link = new HttpLink({
+  uri: `${API_BASE_URL}/graphql`
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+   <App/>
+  </ApolloProvider>,
+  document.getElementById('app')
+);
