2329360378cdb764e5de011f7b79115543e4949e
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-26 16:19:40 +0200

    add login mutation

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,6 +2,7 @@ import React, {Fragment} from "react";
 import Context from "../Context";
 import {UserForm} from "../components/UserForm";
 import {RegisterMutation} from "../container/RegisterMutation";
+import {LoginMutation} from "../container/LoginMutation";
 
 
 export const NotRegisteredUser = () => (
@@ -17,15 +18,26 @@ export const NotRegisteredUser = () => (
                 const variables = {input}
                 register({variables}).then(activateAuth);
               }
-
-              const errorMsg = error && 'El usuario ya existe o ha ocurrido algún problema.';
+              const errorMsg = error && 'El usuario ya existe.';
                return <UserForm error={errorMsg} disabled={loading} onSubmit={onSubmit} title="Registrarse"/>
              }
            }
 
          </RegisterMutation>
 
-         <UserForm onSubmit={activateAuth} title="Iniciar sesión"/>
+         <LoginMutation>
+           {
+             (login, {data, loading, error}) => {
+               const onLogin = ({email, password}) => {
+                 const input = {email, password};
+                 const variables = {input};
+                 login({variables}).then(activateAuth)
+               }
+               const errorMsg = error && 'Usuario y/o contraseña incorrectos.';
+               return <UserForm error={errorMsg} disabled={loading}   onSubmit={onLogin} title="Iniciar sesión"/>
+             }
+           }
+         </LoginMutation>
        </Fragment>
      }
    }
