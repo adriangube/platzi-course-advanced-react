e2468a57daf2bfedde9b03b95eb56d0f1b3f64da
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-26 12:40:53 +0200

    add navigation to detail with @reach/router

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const DETAULT_IMAGE = '/api/assets/category_birds.jpg'
-import {Anchor, Image} from "./styles";
+import {Link, Image} from "./styles";
 
 
-export const Category = ({cover = DETAULT_IMAGE, path, emoji = "?"} ) => (
-	<Anchor href={path}>
+export const Category = ({cover = DETAULT_IMAGE, path="#", emoji = "?"} ) => (
+	<Link to={path}>
     <Image src={cover} alt=""/>
     {emoji}
-	</Anchor>
+	</Link>
 )
