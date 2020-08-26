af5f0342185a4c71533e2e165b39e0aec02d3a8e
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-26 11:20:01 +0200

    chore: add render props to fetch a single photo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {Fragment} from "react";
 import {ListOfCategories} from "./components/ListOfCategories";
 import {Logo} from "./components/Logo";
 
 import {GlobalStyle} from "./styles/GlobalStyles";
 import {ListOfPhotoCards} from "./container/ListofPhotoCards";
+import {PhotoCardWithQuery} from "./container/PhotoCardWithQuery";
 
 
-export const App = () => (
-  <div>
-    <GlobalStyle/>
-    <Logo/>
-    <ListOfCategories/>
-    <ListOfPhotoCards categoryId={1} />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search);
+  const detailId = urlParams.get('detail');
+  return (
+    <div>
+      <GlobalStyle/>
+      <Logo/>
+      {
+        detailId
+        ? <PhotoCardWithQuery id={detailId}/>
+          : <Fragment>
+            <ListOfCategories/>
+            <ListOfPhotoCards categoryId={1} />
+          </Fragment>
+      }
+    </div>
+  )
+}
